1e6ef7fe07905b38882a21075210f582aff53613
Zachary Coursey <zcoursey22@gmail.com>
2018-02-02 01:24:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function nowServing(katzDeliLine) {
 
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) return 'The line is currently empty.';
-  var announcement = 'The line is currently:';
+  var announcement = 'The line is currently: ';
   for (var i = 0; i < katzDeliLine.length; i++) {
     
   }
