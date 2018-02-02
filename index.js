f99f8a8bcf0b6823614190cb23b3b12b32f0e9de
Zachary Coursey <zcoursey22@gmail.com>
2018-02-02 01:15:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  return (`Welcome, ${name}. You are number ${katzDeliLine.length + 1} in line.`); 
+  return (`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`); 
 }
