63dcf96508e1587190b1eedb97b6efe147fc0f48
Zachary Coursey <zcoursey22@gmail.com>
2018-02-02 01:20:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,9 @@ function takeANumber(katzDeliLine, name) {
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return 'There is nobody waiting to be served!';
+  } else {
+    var personBeingServed = katzDeliLine[0];
+    katzDeliLine.shift();
+    return `Currently serving ${personBeingServed}.`;
   }
 }
